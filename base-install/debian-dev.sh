d896e9c9780e69af53f51412bd481eed69d53878
Dain Nilsson <dain@yubico.com>
2015-02-11 12:01:10 +0100

    Set up lintian hook for pbuilder.

--- base-install/debian-dev.sh
+++ base-install/debian-dev.sh
@@ -5,9 +5,28 @@
 
 set -e
 
-PACKAGES="devscripts quilt git-buildpackage debhelper debian-keyring \
+PACKAGES="quilt git-buildpackage debhelper debian-keyring \
   debian-archive-keyring"
 
 cd "$(dirname "$0")"
 
+echo "Installing packages..."
 sudo apt-get install $PACKAGES
+
+echo "Setting up pbuilder hooks..."
+# Setup pbuilder hooks
+HOOKDIR="$HOME/pbuilder-hooks"
+mkdir -p $HOOKDIR
+echo "HOOKDIR=$HOOKDIR" > ~/.pbuilderrc
+
+# Run lintian as part of build
+LINTIAN_HOOK="$HOOKDIR/B90lintian"
+cat << EOF > $LINTIAN_HOOK
+#!/bin/sh
+apt-get -y --force-yes install lintian
+echo +++ lintian output +++
+su -c "lintian -E -v -i -I --pedantic --show-overrides /tmp/buildd/*.changes; :" - pbuilder
+echo +++ end of lintian output +++
+EOF
+
+chmod +x $LINTIAN_HOOK
